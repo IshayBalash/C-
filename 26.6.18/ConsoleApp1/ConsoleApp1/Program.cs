fb91fe08163b7e9109d35a0d7f1f99c86ef57c96
Ishay Balash <35499532+IshayBalash@users.noreply.github.com>
2018-06-28 19:11:20 +0300

    home_work_26.6.18

---
+++ 26.6.18/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NorthwindEntities ef = new NorthwindEntities())
+            {
+                List<Product> ProductAfterDisscount = new List<Product>();
+                foreach(Product p in ef.Products)
+                {
+                    foreach(Category c in ef.Categories)
+                    {
+                        if (p.ProductName.Length == c.CategoryName.Length)
+                        {
+                            if (ProductAfterDisscount.Contains(p)){
+                                continue;
+                            }
+                            else
+                            {
+                                ProductAfterDisscount.Add(p);
+                            }
+                            
+                        }
+                    }
+                }
+                Console.WriteLine($"there is {ProductAfterDisscount.Count} results");
+                foreach (Product p in ProductAfterDisscount)
+                {
+                    Console.WriteLine(p.ProductName);
+                }
+
+                foreach (Product origen in ef.Products)
+                {
+                    for (int i = 0; i < ProductAfterDisscount.Count; i++)
+                    {
+                        if (origen.ProductID == ProductAfterDisscount[i].ProductID)
+                        {
+                            origen.UnitPrice = origen.UnitPrice - (origen.UnitPrice / 10);
+                        }
+                    }
+                }
+
+                ef.SaveChanges();
+                Console.WriteLine("changes were done to the DB");
+
+            }
+                
+            }
+        }
+    }
+
